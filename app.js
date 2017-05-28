48b665923388c4ea273b98b653036eb6d9d539fd
Hideki Akiba <hideki.akiba@tuqulore.com>
2017-05-28 15:10:32 +0900

    fst cmt

--- app.js
+++ app.js
@@ -7,4 +7,4 @@ var http = require("http");
 http.createServer(function (req, res) {
 	res.writeHead(200, {"Content-Type": "text/plain"});
 	res.end("hello, Chikuwa!!!!\n");
-}).listen(1337, "localhost");
+}).listen(process.env.PORT || 8000);
