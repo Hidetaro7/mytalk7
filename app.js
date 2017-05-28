d14aa4aae6d120a199d605a06791f23f01bfdd2f
Hideki Akiba <hideki.akiba@tuqulore.com>
2017-05-28 15:58:14 +0900

    fst cmt

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ var app = express();
 var cors = require('cors');
 var bodyParser = require('body-parser');
 var fs = require('fs');
+var request = require('request');
 var port = process.env.PORT || 8000;
 app.use(cors());
 
@@ -45,9 +46,23 @@ app.get('/', (req, res) => {
 });
 
 
-app.post('/add', (req, res) => {
-	console.log(req.body)
-	res.end(JSON.stringify(req.body));
+app.post('/phrase', (req, res) => {
+	var phrase = req.body.phrase;
+	var url = "https://jlp.yahooapis.jp/KeyphraseService/V1/extract?output=json&appid=dj0zaiZpPTROdVpkbk9hTUduQyZzPWNvbnN1bWVyc2VjcmV0Jng9ODY-&sentence="+phrase;
+	var options = {
+	  url: url,
+	  method: 'GET'/*,
+	  headers: headers,
+	  json: true,
+	  form: {"hoge":"fuga"}*/
+	}
+	request(options, function (error, response, body) {
+	  //コールバックで色々な処理
+	  console.log(body);
+	  res.end(response);
+	})
+	//console.log(req.body)
+	//res.end(JSON.stringify(req.body));
 });
 
 app.listen(port);
