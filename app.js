384497f171edff2b9f39f6e6f598d113456b8dec
Hideki Akiba <hideki.akiba@tuqulore.com>
2017-05-28 16:23:59 +0900

    fst cmt

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use(function(req, res, next) {
 app.get('/', (req, res) => {
 	//res.send('hello');
 	//res.end(JSON.stringify(json));
-	res.sendfile(__dirname + "/index.html");
+	res.sendfile("./index.html");
 });
 
 
